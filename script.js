2743f14e60aa829f834e8fe5671c34b488f3debd
Anton <untilastefan8@gmail.com>
2025-03-13 20:35:00 +0200

    updated magazin.html and added styles/magazin-styles.css

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            alert("Produsul a fost adaugat in cos!");
+        });
+    });
+});

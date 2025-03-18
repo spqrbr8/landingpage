815756d16441c1d687175007dd9a7b45cca33184
Anton <untilastefan8@gmail.com>
2025-03-18 19:20:44 +0200

    added styles/magazin-styles.css, updated pages/magazin.html and js/script.js

--- js/script.js
+++ js/script.js
@@ -10,10 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
     document.documentElement.classList.add('dark-theme');
     toggle.checked = true;
   }
+  
 
   // Eveniment pentru schimbarea temei
   toggle.addEventListener('change', () => {
     document.documentElement.classList.toggle('dark-theme');
     localStorage.setItem('theme', document.documentElement.classList.contains('dark-theme') ? 'dark' : 'light');
   });
+});
+
+// Script -ul pentru magazin
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            alert("Produsul a fost adaugat in cos!");
+        });
+    });
 });
